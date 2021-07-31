b8266f8efb1854e6e92f9925b0863a159f572b08
Aziz <aziko.mz@gmail.com>
2021-07-31 17:28:14 +0300

    Second action start.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import kivy
+from kivy.properties import StringProperty
+
 kivy.require('2.0.0')
+from kivy.uix.button import Button
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 
 class MainWindows(BoxLayout):
-    def button_press(self):
-        return "pressed"
+    def __init__(self, **kwargs):
+        super(MainWindows, self).__init__(**kwargs)
+    # self.my_text = "U!"
+    my_text = StringProperty("1")
+
+    def ons(self):
+        self.my_text = "0"
+        print('Pressed')
+
+    pass
 
 
 class TestApp(App):
