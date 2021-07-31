3c883215ca732df9af3a84e509434665870c6022
Aziz <aziko.mz@gmail.com>
2021-07-31 13:45:36 +0300

    start working

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import kivy
-kivy.require('1.0.7')
-
+kivy.require('2.0.0')
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MainWindows(BoxLayout):
+    def button_press(self):
+        return "pressed"
 
 
 class TestApp(App):
-    pass
+    def build(self):
+        return MainWindows()
 
 
 if __name__ == '__main__':
