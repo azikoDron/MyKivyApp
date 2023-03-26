cd44da59fde2618e5c4bd1581cce5d8990d8329e
Aziz <aziko.mz@gmail.com>
2023-03-26 13:56:58 +0300

    start left nav bar

--- Navigation/top_navbar.py
+++ Navigation/top_navbar.py
@@ -1,11 +1,13 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import StringProperty, ObjectProperty
 Builder.load_file("KV/top_navbar.kv")
 
 
 class TopNavBar(BoxLayout):
+
     button_name = StringProperty("Create")
     # third_button = ObjectProperty()
 
@@ -16,4 +18,8 @@ class TopNavBar(BoxLayout):
         elif self.parent.parent.manager.current == "main_screen":
             self.button_name = "Back"
             self.parent.parent.manager.current = "task_stack_screen"
+    pass
+
+
+class LeftNavBar(FloatLayout):
     pass
